db64ac7185289a1020b5bf1c974c2283197966f0
Patrick Presto <pgpresto@gmail.com>
2019-04-09 23:03:26 +0000

    added S3 upload

--- src/pgbackup/storage.py
+++ src/pgbackup/storage.py
@@ -2,3 +2,9 @@ def local(infine, outfile):
     outfile.write(infile.read())
     outfile.close()
     infile.close()
+
+def s3(client, infile, bucket, name):
+    """
+    https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/s3.html#S3.Client.upload_fileobj
+    """
+    client.upload_fileobj(infile, bucket, name)
